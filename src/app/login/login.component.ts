54aa047d494ed2b8fb0dd45c5a4d20a55caf8852
alfredo <alfredo_9393@live.com.mx>
2020-10-20 15:29:29 -0600

    auth primera parte

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../services/auth.service";
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-root',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user={}
+
+  constructor(private authservice: AuthService,private router:Router) { }
 
   ngOnInit() {
+  }
+
+  singup(){
+    this.authservice.signup(this.user).subscribe(
+      res => {
+        console.log("exito")
+        localStorage.setItem('',res.token)
+        this.router.navigate(['/home']);
+      },
+      err => console.log(err)
+      
+    )
+
   }
 
 }
